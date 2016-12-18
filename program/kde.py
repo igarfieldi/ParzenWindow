37d46098880ed79759455212a8e6e2e248ed765c
Florian Bethe <f.bethe@gmx.de>
2016-12-18 19:19:54 +0100

    Added rudimentary parzen-window classifier with mutlivariate gauss kernel and no bandwidth

--- program/kde.py
+++ program/kde.py
@@ -1,4 +1,104 @@
+import numpy as np
+import math
+
+class KernelDensityEstimator:
+    def __init__(self, classes):
+        self.trainingFeatures = [[] for i in range(classes)];
+
+    def addTrainingInstance(self, instance, label):
+        """
+        Adds a single instance with its given label to the training set.
+        :param instance: Feature vector
+        :param label: Label of the instance
+        """
+        self.trainingFeatures[label].append(instance);
+
+
+    def addTrainingInstances(self, instances, labels):
+        """
+        Adds multiple instances with their labels as a list to the training set.
+        :param instances: List of instances
+        :param labels: List of corresponding labels
+        """
+        for i in range(len(labels)):
+            self.trainingFeatures[labels[i]].append(instances[i]);
+
+    def classify(self, instances):
+        """
+        Computes the class probabilities for a given set of instances.
+        The probabilities will be returned as a matrix with each class and instance combination.
+        For the computation, the stored training instances will be used.
+
+        :param instances: List of instances (ie. vectors of numeric features)
+        :return: Matrix containing the class probabilities
+        """
+        priors = [0] * len(self.trainingFeatures);
+        probabilities = [[0 for x in range(len(priors))] for y in range(len(instances))];
+        print(probabilities);
+
+        # For all class labels estimate the prior of the class
+        for i in range(len(self.trainingFeatures)):
+            priors[i] = len(self.trainingFeatures[i]);
+
+        # Now we iterate over all instances to determine the kernel densities
+        for i in range(len(instances)):
+            densities = [0] * len(self.trainingFeatures);
+
+            # Then estimate the kernel density based on the stored instances with that label
+            # TODO: variable kernel, incorporate bandwidth!
+            for j in range(len(self.trainingFeatures)):
+                densities[j] = estimateKernelDensity(instances[i], self.trainingFeatures[j], gaussKernel, 0);
+
+            # Compute probabilities as product of estimated prior and density
+            for j in range(len(densities)):
+                probabilities[i][j] = priors[j]*densities[j];
+
+            # Normalize probabilities
+            probabilities[i] /= sum(probabilities[i]);
+
+        return probabilities;
+
 def kde(data):
-    import numpy as np
     len = len(data)
     return 0
+
+def estimateKernelDensity(instance, samples, kernel, bandwidth):
+    """
+    Returns the multivariate kernel density estimation for the given instance.
+    The covariance for the kernel is estimated from the samples.
+    :param instance: Instance to estimate density for
+    :param samples: List of feature vectors to base estimation on
+    :param kernel: Kernel to use for the estimation
+    :param bandwidth: Smoothing matrix for the kernel
+    :return: Estimated kernel density for the instance
+    """
+
+    # Get the covariance matrix from the samples
+    cov = np.cov(samples, None, False);
+
+    density = 0;
+
+    # Normal density estimation: for every sample compute kernel and sum up
+    for i in range(0, np.shape(samples)[0]):
+        density += kernel(instance - samples[i], cov);
+    density /= len(samples);
+
+    return density;
+
+def gaussKernel(x, cov):
+    return np.exp(-0.5 * np.dot(np.dot(x, np.linalg.inv(cov)), x.transpose())) /\
+           math.sqrt(math.pow(2.0*math.pi, x.size) * np.linalg.det(cov));
+
+
+trainingData = [np.array([0, 0]),
+                np.array([0, 1]),
+                np.array([1, 0]),
+                np.array([1, 0.5]),
+                np.array([0.5, 1]),
+                np.array([1, 1])];
+trainingLabels = [0, 0, 0, 1, 1, 1];
+testData = [np.array([0, 0]), np.array([0.5, 0.5]), np.array([1, 1])];
+
+estimator = KernelDensityEstimator(2);
+estimator.addTrainingInstances(trainingData, trainingLabels);
+print(estimator.classify(testData));
