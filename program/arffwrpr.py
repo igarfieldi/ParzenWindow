8db296b0cc6a13a9636fd25ca318154d68c9e70d
Angelika Ophagen <ophagen@st.ovgu.de>
2016-12-22 20:32:15 +0100

    arff  wrapper, first working version - do we want integer conversion?

--- program/arffwrpr.py
+++ program/arffwrpr.py
@@ -1,9 +1,41 @@
+import numpy as np
 from scipy.io.arff import loadarff
 
 
 class ArffWrapper:
     def __init__(self, filename):
         with open(filename, 'r') as f:
-            self.data, self.meta = loadarff(f)
+            self.data, self.meta = loadarff(f);
 
+    def trainingData(self):
+        """
+        Returns the data from the ARFF file as numpy array of arrays,
+        without the label information
+        :rtype: array of arrays with instance values
+        """
+        # everything but the last column
+        trainingData = self.data[self.meta.names()[:-1]];
+        # converts the record array to a numpy array
+        trainingData = trainingData.view(np.float)\
+            .reshape(self.data.shape + (-1,));
+        return trainingData;
 
+    def trainingLabels(self):
+        """
+        Returns only the label data from the ARFF file, converted to integer as a numpy array
+        :return: numpy array of integer label entries
+        """
+        trainingLabels = [];
+        # only the last column
+        for x in self.data[self.meta.names()[-1:]]:
+            trainingLabels.append(np.int(x[0]));
+        # converts the integer list to a numpy array
+        trainingLabels = np.array(trainingLabels);
+        return trainingLabels;
+
+myArff = ArffWrapper('testdata/iris-testdata.arff');
+print myArff.meta.names();
+print myArff.meta.types();
+print myArff.data;
+print myArff.trainingLabels();
+print myArff.trainingData();
