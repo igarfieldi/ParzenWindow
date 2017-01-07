Fixed up MCMC ergodic averaging

program/bandwidth.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy
 import math
 from kernel import GaussKernel
 from mcmc import metropolisHastingsSampling
@@ -31,35 +32,43 @@ def estimateLikelihood(kde, choleskyBandwidth):
     return likelihood
 
 class McMcBandwidthEstimator:
-    def __init__(self, shape, priors, expLambda=5.0, iterations=2000):
+    def __init__(self, dims, shape=1, priors=None, iterations=2500, burnIn=500, beVerbose=False):
         self.shape = shape
-        self.priors = priors
-        self.iterations = iterations
+        if priors is None:
+            self.priors = np.identity(dims, dtype=np.float64)
+        else:
+            if np.shape(priors) != (dims, dims):
+                raise ValueError('The priors must have the same dimensionality as the bandwidth!')
+            else:
+                self.priors = priors
+        self.iterations = max(1, iterations)
+        self.burnIn = min(self.iterations, burnIn)
+        self.beVerbose = beVerbose
+
         # Proposed distribution: will be used in Metropolis-Hastings; for now leave as multivariate gauss
         # TODO: what's better; discarding the samples out-of-bounds or using independence sampling?
-        #self.proposed = lambda theta, theta_p: GaussKernel(np.identity(len(theta[np.tril_indices(len(theta))]))).eval(theta[np.tril_indices(len(theta))])
-        self.proposed = lambda theta, theta_p: np.prod(expLambda * np.exp(-expLambda * theta[np.tril_indices(len(theta))]))
+        self.proposed = lambda theta, theta_p: GaussKernel(np.identity(len(theta[np.tril_indices(len(theta))]))).eval(theta[np.tril_indices(len(theta))])
         # Sampler for the proposed distribution (generates new samples for the algorithm)
-        self.proposedSampler = lambda theta_p: np.random.exponential(expLambda, (len(theta_p), len(theta_p))) * np.tril(np.ones(theta_p.shape))
+        self.proposedSampler = lambda theta_p: theta_p + np.random.normal(0, 1, (len(theta_p), len(theta_p)))* np.tril(np.ones(theta_p.shape))
 
     def estimateBandwidth(self, kde):
         # Target function (only proportional to what we really have as a distribution):
         # This is the product of the prior and the likelihood (see the paper for more detailed explanation)
         target = lambda theta: estimatePrior(theta, self.shape) * estimateLikelihood(kde, theta)
 
-        bandwidths, acceptance = metropolisHastingsSampling(self.priors, self.iterations, target, self.proposed,
+        bandwidths, accepted = metropolisHastingsSampling(self.priors, self.iterations, target, self.proposed,
                                                 self.proposedSampler)
 
         optimalBandwidth = np.zeros(self.priors.shape)
 
-        # Attempt at computing the posterior mean
-        # TODO: I'm pretty sure this is wrong...
-        for b in bandwidths[self.iterations/2:]:
-            optimalBandwidth += b * target(b)
+        # Attempt at computing the ergodic average
+        for b in bandwidths[self.burnIn:]:
+            optimalBandwidth += b
+        optimalBandwidth /= float(self.iterations - self.burnIn)
 
-        print(acceptance)
-        print(optimalBandwidth)
-        print(np.dot(optimalBandwidth, optimalBandwidth.transpose()))
+        if self.beVerbose:
+            print('Acceptance: {} out of {} ({}%)').format(accepted, self.iterations, 100.0 * accepted / float(self.iterations))
+            print('Optimal bandwidth:\n{}').format(np.dot(optimalBandwidth, optimalBandwidth.transpose()))
 
         return optimalBandwidth
 
@@ -78,6 +87,8 @@ class SilvermanBandwidthEstimator:
         """
         d = self.cov.shape[0]
         n = len(kde.getSamples())
+
+        print(d, n)
         bandwidth = np.zeros(self.cov.shape)
 
         for i in range(d):

program/kde.py
@@ -21,8 +21,8 @@ class KernelDensityEstimator:
         self.samples.append(sample)
 
     def addSamples(self, samples):
-        for i in samples:
-            self.samples.append(i)
+        for sample in samples:
+            self.samples.append(sample)
 
     def getSamples(self):
         return self.samples
@@ -42,6 +42,9 @@ class KernelDensityEstimator:
 
     def estimateDensity(self, instance, choleskyBandwidth):
         bandwidthDet = np.linalg.det(choleskyBandwidth)
+
+        if len(self.samples) == 0:
+            return 0
 
         density = 0
 
@@ -117,22 +120,32 @@ class ParzenWindowClassifier:
 
         return probabilities
 
-
 from sckde import SelfConsistentKDE
 
 testFile = ArffWrapper('testdata/iris-testdata.arff');
 
-data = np.array([[-0.5, 0.25, 0.5, -0.25, 0, -0.25, 0.5],
-                 [-1.5, 1.25, 1.5, -1.25, 0, -1.25, 1.5]])
+training = np.random.permutation(len(testFile.trainingData()))
+test = training[:len(training)/2]
+training = training[len(training)/2+1:]
 
 # TODO: fix wrong transposition!
 #selfConsistent = SelfConsistentKDE(testFile.trainingData().transpose(), 1)
 #print(selfConsistent.phiSC)
 
-priors = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
-
 # TODO: bandwidth estimate must not be below zero!
-classifier = ParzenWindowClassifier(GaussKernel(np.cov(testFile.trainingData(), None, False)), testFile.labelCount())
-classifier.addTrainingInstances(testFile.trainingData(), testFile.trainingLabels())
-classifier.estimateBandwidths(McMcBandwidthEstimator(0.25, priors, 4.0, 1000))
-print(classifier.classify([testFile.trainingData()[15]]))
+classifier = ParzenWindowClassifier(GaussKernel(np.cov(testFile.trainingData()[training], rowvar=False)), testFile.labelCount())
+classifier.addTrainingInstances(testFile.trainingData()[training], testFile.trainingLabels()[training])
+classifier.estimateBandwidths(McMcBandwidthEstimator(dims=testFile.dimensions(), beVerbose=True))
+#classifier.estimateBandwidths(SilvermanBandwidthEstimator(np.cov(testFile.trainingData()[training], rowvar=False)))
+
+acc = 0
+total = 0
+
+for i in range(len(test)):
+    probs = classifier.classify([testFile.trainingData()[test[i]]])
+
+    if(probs.index(max(probs)) == testFile.trainingLabels()[test[i]]):
+        acc += 1
+    total += 1
+
+print(acc / float(total))

program/mcmc.py
@@ -9,13 +9,13 @@ def metropolisHastingsSampling(parameters, iterations, target, proposed, propose
     :param target: target function with proportional distribution to posterior
     :param proposed: proposed distribution to sample from
     :param step: distribution to draw step size from
-    :return: list of samples for parameters (it is wise to discard an initial percentage!), acceptance rate of samples
+    :return: list of samples for parameters (it is wise to discard an initial percentage!), number of accepted samples
     """
     samples = [np.zeros(len(parameters))] * (iterations + 1);
     samples[0] = parameters;
 
     # Tracks the number of accepted samples
-    acceptance = 0;
+    accepted = 0;
 
     for i in range(iterations):
         # Get new proposed sample from sampler
@@ -28,10 +28,10 @@ def metropolisHastingsSampling(parameters, iterations, target, proposed, propose
         u = np.random.uniform();
         if u < rho:
             parameters = parameters_p;
-            acceptance += 1;
+            accepted += 1;
 
         # Regardless of whether we accepted the sample or not, the current sample point still counts as an element
         # in the resulting markov chain
         samples[i+1] = parameters;
 
-    return samples, acceptance / float(iterations);
+    return samples, accepted;